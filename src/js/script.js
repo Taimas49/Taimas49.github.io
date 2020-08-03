cd8e893f22e0384262cf55c00b9be1b29fcf0bc6
Taimas <urazgalit@gmail.com>
2020-08-04 01:45:55 +0600

    added messages

--- src/js/script.js
+++ src/js/script.js
@@ -75,7 +75,37 @@ $(document).ready(function(){
       validateForms('#consultation-form');
       validateForms('#consultation form');
       validateForms('#order form');
+//Письма на почту
 
+      $('input[name=phone]').mask("+7 (999) 999-99-99");
+      
+      $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+          type: "POST",
+          url: "mailer/smart.php",
+          data: $(this).serialize()
+        }).done(function() {
+          $(this).find("input").val("");
+          $('consultation, #order').fadeOut();
+          $('.overlay, #thanks').fadeIn('slow');
 
-      $('input[name=phone]').mask("+7 (999) 999-99-99"); 
-    });
+          $("form").trigger('reset');
+        });
+        return false;
+      });
+//Плавный скролл и кнопка "Вверх"
+
+      $(window).scroll(function(){
+        if ($(this).scrollTop() > 1600){
+          $('.pageup').fadeIn();
+        } else {
+          $('.pageup').fadeOut();
+        }
+      })
+      $("a[href^='#']").click(function(){
+        var _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+});
+  });
